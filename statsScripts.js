8a4e0564ed1ae8d2aa8ff52f84e0c2c07d249198
Jolijn Rosendaal <jolijnrosendaal@gmail.com>
2024-05-28 15:19:42 +0200

    amount increaser working

--- statsScripts.js
+++ statsScripts.js
@@ -1,3 +1,23 @@
+function getBackedAmount() {
+    // get the amount that was already backed
+    let backedAmount = document.querySelector('#backed-amount').innerHTML;
+    // convert string to a number
+    backedAmount = backedAmount.substring(1);
+    backedAmount = backedAmount.split(',').join('');
+    backedAmount = Number(backedAmount);
+    return backedAmount;
+}
+function updateDOM(backedAmount) {
+    // convert backedAmount to a string
+    let backedAmountString = backedAmount.toString();
+    //place the ',' back
+    backedAmountString = backedAmountString.slice(0, backedAmountString.length - 3) + ',' + backedAmountString.slice(-3);
+    //place the $ back
+    backedAmountString = '$' + backedAmountString;
+    // update the DOM with the new backedAmount
+    document.querySelector('#backed-amount').innerHTML = backedAmountString;
+}
+
 // continue button function
 const continueBtns = document.querySelectorAll('.confirm__continue-btn');
 continueBtns.forEach((btn) => {
@@ -11,35 +31,20 @@ continueBtns.forEach((btn) => {
         const idNum = btn.id.slice(-1);
         // get pledge amount from input with the right id
         const amount = Number(document.querySelector('#confirm__input-num' + idNum).value);
-        // get the amount that was already backed
-        let backedAmount = document.querySelector('#backed-amount').innerHTML;
-        // convert string to a number
-        backedAmount = backedAmount.substring(1);
-        backedAmount = backedAmount.split(',').join('');
-        backedAmount = Number(backedAmount);
-        backedAmountNew = backedAmount + amount;
 
         // start animation increase backedAmountString
         let counter = 0;
+        let backedAmount = getBackedAmount();
         let amountIncreaser;
 
         amountIncreaser = setInterval(() => {
             counter++;
-            console.log('hi Jolijn');
+            backedAmount++;
+            updateDOM(backedAmount);
 
-            if (counter === 3) {
+            if (counter === amount) {
                 clearInterval(amountIncreaser);
             }
-        }, 3000);
-
-        // convert backedAmount to a string
-        backedAmountString = backedAmountNew.toString();
-        //place the ',' back
-        backedAmountString = backedAmountString.slice(0, backedAmountString.length - 3) + ',' + backedAmountString.slice(-3);
-        //place the $ back
-        backedAmountString = '$' + backedAmountString;
-
-        // update the DOM with the new backedAmount
-        //document.querySelector('#backed-amount').innerHTML = backedAmountString;
+        }, 100);
     };
 });
